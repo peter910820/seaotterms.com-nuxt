b790c0fea6a121d770a2ad2ec147711b2eccf5a3
peter910820 <peter910820@gmail.com>
2025-07-27 02:58:48 +0800

    fix: fix error of brand create and index ssr

--- pages/brand/create.vue
+++ pages/brand/create.vue
@@ -46,7 +46,7 @@ const handleSubmit = async () => {
 
   try {
     const response = await $fetch<CommonResponse>("brand", {
-      baseURL: import.meta.env.VITE_API_URL,
+      baseURL: import.meta.env.VITE_API_INTERNAL_URL,
       method: "POST",
       body: request.value,
     });
