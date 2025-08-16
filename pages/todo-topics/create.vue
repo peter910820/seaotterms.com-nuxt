76e03c54bcbdef2601c7bb33e5e4919d580e72b6
peter910820 <peter910820@gmail.com>
2025-08-16 11:27:56 +0800

    feat: add create system todo topic feature

--- pages/todo-topics/create.vue
+++ pages/todo-topics/create.vue
@@ -24,24 +24,52 @@ const form = ref<TodoTopicCreateRequest>({
   updateName: user.value.username,
 });
 
-const handleSubmit = async () => {
-  if (form.value.topicName.trim() === "") {
-    alert("標題不得為空");
-    return;
-  }
+const form2 = ref<TodoTopicCreateRequest>({
+  topicName: "",
+  topicOwner: "system",
+  updatedAt: new Date(),
+  updateName: user.value.username,
+});
 
-  try {
-    const response = await $fetch<CommonResponse>("todo-topics", {
-      baseURL: import.meta.env.VITE_API_URL,
-      method: "POST",
-      body: form.value,
-      credentials: "include",
-    });
-    userInfoHandler(response.userInfo);
-    messageStorage(response.statusCode, "資料創建成功");
-    router.push("/message");
-  } catch (error) {
-    errorHandler(error);
+const handleSubmit = async (isSystem: boolean) => {
+  if (isSystem) {
+    if (form2.value.topicName.trim() === "") {
+      alert("標題不得為空");
+      return;
+    }
+
+    try {
+      const response = await $fetch<CommonResponse>("todo-topics", {
+        baseURL: import.meta.env.VITE_API_URL,
+        method: "POST",
+        body: form2.value,
+        credentials: "include",
+      });
+      userInfoHandler(response.userInfo);
+      messageStorage(response.statusCode, "資料創建成功");
+      router.push("/message");
+    } catch (error) {
+      errorHandler(error);
+    }
+  } else {
+    if (form.value.topicName.trim() === "") {
+      alert("標題不得為空");
+      return;
+    }
+
+    try {
+      const response = await $fetch<CommonResponse>("todo-topics", {
+        baseURL: import.meta.env.VITE_API_URL,
+        method: "POST",
+        body: form.value,
+        credentials: "include",
+      });
+      userInfoHandler(response.userInfo);
+      messageStorage(response.statusCode, "資料創建成功");
+      router.push("/message");
+    } catch (error) {
+      errorHandler(error);
+    }
   }
 };
 </script>
@@ -58,8 +86,26 @@ const handleSubmit = async () => {
           <label for="title">title</label>
         </div>
         <div class="col s12">
-          <button @click="handleSubmit" class="button-submit" type="button">
+          <button @click="handleSubmit(false)" class="button-submit" type="button">
             建立主題
+            <i class="material-icons right">send</i>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <h1>建立系統站台</h1>
+    <div class="col s12 sub-block wow animate__flipInX">
+      <div class="row">
+        <div class="input-field col s12">
+          <i class="material-icons prefix">title</i>
+          <input id="title" v-model="form2.topicName" type="text" class="validate" required />
+          <span class="helper-text" data-error="此欄不能為空" data-success=""></span>
+          <label for="title">title</label>
+        </div>
+        <div class="col s12">
+          <button @click="handleSubmit(true)" class="button-submit" type="button">
+            建立系統站台
             <i class="material-icons right">send</i>
           </button>
         </div>
@@ -70,7 +116,7 @@ const handleSubmit = async () => {
 
 <style lang="scss" scoped>
 .sub-block {
-  min-height: 200px;
+  min-height: 400px;
   height: auto;
 }
 </style>
